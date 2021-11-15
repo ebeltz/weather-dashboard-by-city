63cf9e3bd115da44a4f9effb4a0e99d03c988794
ebeltz <ebbeltz@gmail.com>
2021-11-14 23:12:51 -0500

    start coding the script.js file

---
+++ Assets/js/script.js
@@ -0,0 +1,5 @@
+function initPage() {
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+}
