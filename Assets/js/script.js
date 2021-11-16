e97b77e8e7a4a98ba84dfa63556ce565aee92ddd
ebeltz <ebbeltz@gmail.com>
2021-11-15 21:36:56 -0500

    added 5 day forecast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -30,6 +30,66 @@ function initPage() {
             nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
             let weatherPic = response.data.weather[0].icon;
             currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-        })
+            currentPicEl.setAttribute("alt", response.data.weather[0].description);
+            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&#176F";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+            // get UV Index
+            let lat = response.data.coord.lat;
+            let lon = response.data.coord.lon;
+            let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+            axios.get(UVQueryURL).then(function(response) {
+                let UVIndex = document.createElement("span");
+
+                // good UV Index = green, ok UV Index = yellow, and bad UV Index = red
+                if (response.data[0].value < 4) {
+                    UVIndex.setAttribute("class", "badge badge-success");
+                }
+                else if (response.data[0].value < 8) {
+                    UVIndex.setAttribute("class", "badge bage-warning");
+                }
+                else {
+                    UVIndex.setAttribute("class", "badge badge-danger");
+                }
+                console.log(response.data[0].value)
+                UVIndex.innerHTML = response.data[0].value;
+                currentUVEl.innerHTML = "UV Index: ";
+                currentUVEl.append(UVIndex);
+            });
+
+            // 5 day forecast by city
+            let cityID = response.data.id;
+            let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+            axios.get(forecastQueryURL).then(function(response) {
+                fivedayEl.classList.remove("d-none");
+
+                // next 5 day forecast displayed through parsed response
+                const forecastELs = document.querySelectorAll(".forecast");
+                for (i = 0; i < forecastELs.length; i++) {
+                    const forecastIndex = i * 8 + 4;
+                    const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                    const forecastDay = forecastDate.getDate();
+                    const forecastMonth = forecastDate.getMonth() + 1;
+                    const forecastYear = forecastDate.getFullYear();
+                    const forecastDateEl = document.createElement("p");
+                    forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                    forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                    forecastEls[i].append(forecastDateEl);
+
+                    // current weather icons
+                    const forecastWeatherEl = document.createElement("img");
+                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                    forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+                    forecastEls[i].append(forecastWeatherEl);
+                    const forecastTempEl = document.createElement("p");
+                    forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
+                    forecastEls[i].append(forecastTempEl);
+                    const forecastHumidityEl = document.createElement("p");
+                    forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                    forecastEls[i].append(forecastHumidityEl);
+                }
+            })
+        });
     }
 }
